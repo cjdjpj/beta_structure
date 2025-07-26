89dac4936f220d96637c72c3251f557f64211b2c
cjdjpj <cjdjpj@hotmail.com>
2025-07-26 17:48:53 +1000

    v1 figures

--- manuscript_figures/kingman_progression.py
+++ manuscript_figures/kingman_progression.py
@@ -4,8 +4,8 @@ import pickle
 import seaborn as sns
 import matplotlib.pyplot as plt
 import matplotlib.ticker as mticker
+from sklearn.manifold import MDS
 from scipy.spatial.distance import squareform
-from skbio.stats.ordination import pcoa
 import scienceplots
 
 plt.style.use("science")
@@ -20,7 +20,7 @@ plt.rcParams.update({
 })
 
 save_fig = True
-run_indices = ["r001", "r002", "r004"]
+run_indices = ["r001", "r002", "r004", "r008"]
 
 def load_run(run_index):
     input_path = "runs/" + run_index
@@ -45,7 +45,7 @@ def load_run(run_index):
 
     recomb_status = [
         "Fully recombined" if frac == 0 
-        else "Partially recombined" if 0 < frac < 1 
+        else "Partially\nrecombined" if 0 < frac < 1 
         else "Fully clonal" 
         for frac in frac_clonal
     ]
@@ -55,47 +55,39 @@ def load_run(run_index):
 # CREATE FIGURE
 fig, axes = plt.subplot_mosaic(
     [
-        ["A", "A", "B", "B", "C", "C"],
-        ["A", "A", "B", "B", "C", "C"],
+        ["A", "A", "B", "B", "C", "C", "D", "D"],
+        ["A", "A", "B", "B", "C", "C", "D", "D"],
     ],
-    figsize = (7.2, 2.4),
+    figsize = (8, 2),
     sharey = True
 )
 
 plt.subplots_adjust(wspace=0.3, hspace=0.15)
 
-for ax, run_index, label in zip(axes.values(), run_indices, [r"$\textbf{A}$", r"$\textbf{B}$", r"$\textbf{C}$"]):
+for ax, run_index, label in zip(axes.values(), run_indices, [r"$\textbf{A}$", r"$\textbf{B}$", r"$\textbf{C}$", r"$\textbf{D}$"]):
     dist, recomb_status, r_d, params = load_run(run_index)
 
     sns.histplot(
         x=dist, stat="probability", hue=recomb_status,
-        bins=40, multiple="stack", hue_order=["Partially recombined", "Fully recombined", "Fully clonal"],
-        ax=ax, legend = (ax == axes["C"])
+        bins=40, multiple="stack", hue_order=["Partially\nrecombined", "Fully recombined", "Fully clonal"],
+        ax=ax, legend = (ax == axes["D"])
     )
 
-    if ax == axes["A"]:
-        inset_ax = ax.inset_axes([0.05, 0.45, 0.35, 0.35])
+    inset_ax = ax.inset_axes([0.05, 0.45, 0.35, 0.35])
 
-        dist_matrix = squareform(dist)
-        pcoa_results = pcoa(dist_matrix)
+    dist_matrix = squareform(dist)
 
-        ### 2D PCA
-        pcoa_coords = pcoa_results.samples[["PC1", "PC2"]].values
-        variance_explained = pcoa_results.proportion_explained
+    mds = MDS(n_components=2, dissimilarity='precomputed', random_state=42)
+    mds_coords = mds.fit_transform(dist_matrix)
 
-        jitter_strength = 0.001
-        jittered_coords = pcoa_coords + np.random.normal(loc=0, scale=jitter_strength, size=pcoa_coords.shape)
+    sns.scatterplot(x=mds_coords[:, 0], y=mds_coords[:, 1],
+                    ax=inset_ax, color=sns.color_palette()[5],
+                    edgecolor="white", linewidth=0.2, s=12)
 
-        sns.scatterplot(x=jittered_coords[:, 0], y=jittered_coords[:, 1],
-                        ax = inset_ax, color = sns.color_palette()[2],
-                        edgecolor="black",
-                        linewidth = 0.2,
-                        s = 15)
-
-        inset_ax.set_xticklabels([])
-        inset_ax.set_yticklabels([])
-        inset_ax.set_xlabel("")
-        inset_ax.set_ylabel("")
+    inset_ax.set_xticklabels([])
+    inset_ax.set_yticklabels([])
+    inset_ax.set_xlabel("")
+    inset_ax.set_ylabel("")
 
     ax.text(-0.1, 1.1, label, transform=ax.transAxes, 
             fontweight="bold", va="top", ha="left")
@@ -103,22 +95,22 @@ for ax, run_index, label in zip(axes.values(), run_indices, [r"$\textbf{A}$", r"
     ax.text(0.05, 0.95, f"$\\bar r_d$ = {r_d:.3f}", transform=ax.transAxes,
             verticalalignment="top")
     
-    ax.set_title(f"$\\rho/\\mu = {round(params["r_m"], 3)}$")
+    rho = params["r_m"] * params["track_length"] * params["pi"]/(2)
+    ax.set_title(f"$\\rho = {rho:.4g}$")
     ax.set_xlabel("")
 
     if ax != axes["A"]:
         ax.set_ylabel("")
 
-    formatter = mticker.FormatStrFormatter("%.2f")
-    ax.xaxis.set_major_formatter(formatter)
-    ax.yaxis.set_major_formatter(formatter)
+    ax.xaxis.set_major_formatter(mticker.FormatStrFormatter("%.2f"))
+    ax.yaxis.set_major_formatter(mticker.FormatStrFormatter("%.1f"))
 
-sns.move_legend(axes["C"], "center left")
+sns.move_legend(axes["D"], "lower left")
 
-fig.text(0.5, 0.02, "Pairwise mean number of nucleotide differences (Nei's $\\pi$)", ha="center")
+fig.text(0.5, 0.00, "Pairwise mean number of nucleotide differences", ha="center")
 fig.subplots_adjust(left=0.15, bottom=0.15)
 
 if save_fig:
-    plt.savefig("../figures/kingman_progression.png", dpi=300, bbox_inches = "tight")
+    plt.savefig("../figures/kingman_progression.png", dpi=500, bbox_inches = "tight")
 else:
     plt.show()
--- manuscript_figures/r_d_distribution.py
+++ manuscript_figures/r_d_distribution.py
@@ -45,41 +45,63 @@ cdf2 = pd.concat([df3, df7], ignore_index=True)
 cdf3 = pd.concat([df4, df8], ignore_index=True)
 cdfs = [cdf1, cdf2, cdf3]
 
-titles = [r"$\rho/\mu = 0.0$", r"$\rho/\mu = 0.01$", r"$\rho/\mu = 0.1$"]
+titles = [r"$\rho = 0$", r"$\rho = 0.75$", r"$\rho = 7.5$"]
 
 # CREATE FIGURE
 fig, axes = plt.subplot_mosaic(
     [
         ["A", "A", "B", "B", "C", "C"],
         ["A", "A", "B", "B", "C", "C"],
+        ["a", "a", "b", "b", "c", "c"],
     ],
-    figsize = (7.2, 2.4),
-    sharey = True
+    figsize = (6, 3),
+    sharex = True
 )
 
 plt.subplots_adjust(wspace=0.3, hspace=0.15)
 
-for ax, cdf, title, label in zip(axes.values(), cdfs, titles, [r"$\textbf{A}$", r"$\textbf{B}$", r"$\textbf{C}$"]):
+for ax, cdf, title, label in zip([axes["A"], axes["B"], axes["C"]], cdfs, titles, [r"$\textbf{A}$", r"$\textbf{B}$", r"$\textbf{C}$"]):
 
+    bins = 30
     sns.histplot(
-        data=cdf, x=cdf.columns[0], bins=40, hue="model", stat="probability",
+        data=cdf, x=cdf.columns[0], bins=30, hue="model", stat="probability",
         multiple="layer", ax=ax, legend = (ax == axes["C"])
     )
 
     ax.text(-0.1, 1.1, label, transform=ax.transAxes, 
             fontweight="bold", va="top", ha="left")
 
-    ax.set_xlim(-0.01, 0.31)
+    ax.set_xlim(-0.01, 0.61)
+    ax.set_ylim(0.00, 0.53)
     ax.set_title(title)
     ax.set_xlabel("")
+    ax.set_ylabel("")
     if ax != axes["A"]:
-        ax.set_ylabel("")
+        ax.set_yticklabels([])
+
+for ax, cdf in zip([axes["a"], axes["b"], axes["c"]], cdfs):
+
+    bins = 30
+    sns.histplot(
+        data=cdf, x=cdf.columns[0], bins=30, hue="model", stat="probability",
+        multiple="layer", ax=ax, legend = False
+    )
+
+    ax.set_yscale("log")
+    ax.set_xlim(-0.01, 0.61)
+    ax.set_ylim(3e-4, 0.99)
+    ax.set_xlabel("")
+    ax.set_ylabel("")
+    ax.tick_params(axis='y', which='major', labelsize=6)
+    if ax != axes["a"]:
+        ax.set_yticklabels([])
 
 
-fig.text(0.5, 0.02, "$\\bar r_d$", ha="center")
+fig.text(0.5, 0.07, "$\\bar r_d$", ha="center")
+fig.text(0.09, 0.5, "Probability", va="center", rotation="vertical")
 fig.subplots_adjust(left=0.15, bottom=0.15)
 
 if save_fig:
-    plt.savefig("../figures/r_d_distribution.png", dpi=300, bbox_inches = "tight")
+    plt.savefig("../figures/r_d_distribution.png", dpi=500, bbox_inches = "tight")
 else:
     plt.show()
